169af4e1f2f77cbfd5d500cf93353a281f55252a
nanzejiangcheng4 <nanzejiangcheng4@gmail.com>
2025-06-14 16:43:37 +0900

    会員をすべて取得するapi実装

---
+++ interfaces.ts
@@ -0,0 +1,11 @@
+export interface Member {
+  id: number;
+  name: string;
+  email: string;
+  points: number;
+  note?: string;
+}
+export interface ReturnJSONMembers {
+  result: number;
+  data: Member[];
+}
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <header>
+    <h1>サーバーサイドルーティングサンプル</h1>
+  </header>
+  <main>
+    <slot />
+  </main>
+</template>
